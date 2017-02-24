768c6d71a58f324ed63709e65683be6a0d69d88c
Michael Zhang <yidongnan@gmail.com>
2017-02-24 16:29:15 +0800

    *  optimization_channel_factory

--- grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/AddressChannelFactory.java
+++ grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/AddressChannelFactory.java
@@ -1,12 +1,16 @@
 package net.devh.springboot.autoconfigure.grpc.client;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import com.google.common.collect.Lists;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import io.grpc.Channel;
+import io.grpc.ClientInterceptor;
+import io.grpc.ClientInterceptors;
 import io.grpc.LoadBalancer;
-import io.grpc.ManagedChannel;
 import io.grpc.NameResolver;
 import io.grpc.netty.NettyChannelBuilder;
 
@@ -19,32 +23,37 @@ public class AddressChannelFactory implements GrpcChannelFactory {
     private final GrpcChannelsProperties properties;
     private final LoadBalancer.Factory loadBalancerFactory;
     private final NameResolver.Factory nameResolverFactory;
+    private final GlobalClientInterceptorRegistry globalClientInterceptorRegistry;
 
-    private static Map<String, ManagedChannel> channelMap = new ConcurrentHashMap<>();
-
-    public AddressChannelFactory(GrpcChannelsProperties properties, LoadBalancer.Factory loadBalancerFactory) {
+    public AddressChannelFactory(GrpcChannelsProperties properties, LoadBalancer.Factory loadBalancerFactory, GlobalClientInterceptorRegistry globalClientInterceptorRegistry) {
         this.properties = properties;
         this.loadBalancerFactory = loadBalancerFactory;
         this.nameResolverFactory = new AddressChannelResolverFactory(properties);
+        this.globalClientInterceptorRegistry = globalClientInterceptorRegistry;
     }
 
     @Override
     public Channel createChannel(String name) {
-        ManagedChannel channel = channelMap.get(name);
-        if (channel == null) {
-            synchronized (channelMap) {
-                if (channelMap.get(name) == null) {
-                    GrpcChannelProperties channelProperties = properties.getChannel(name);
-                    channel = NettyChannelBuilder.forTarget(name)
-                            .loadBalancerFactory(loadBalancerFactory)
-                            .nameResolverFactory(nameResolverFactory)
-                            .usePlaintext(channelProperties.isPlaintext())
-                            .enableKeepAlive(channelProperties.isEnableKeepAlive(), channelProperties.getKeepAliveDelay(), TimeUnit.SECONDS, channelProperties.getKeepAliveTimeout(), TimeUnit.SECONDS)
-                            .build();
-                    channelMap.put(name, channel);
-                }
-            }
+        return this.createChannel(name, null);
+    }
+
+    @Override
+    public Channel createChannel(String name, List<ClientInterceptor> interceptors) {
+        GrpcChannelProperties channelProperties = properties.getChannel(name);
+        Channel channel = NettyChannelBuilder.forTarget(name)
+                .loadBalancerFactory(loadBalancerFactory)
+                .nameResolverFactory(nameResolverFactory)
+                .usePlaintext(channelProperties.isPlaintext())
+                .enableKeepAlive(channelProperties.isEnableKeepAlive(), channelProperties.getKeepAliveDelay(), TimeUnit.SECONDS, channelProperties.getKeepAliveTimeout(), TimeUnit.SECONDS)
+                .build();
+        List<ClientInterceptor> globalInterceptorList = globalClientInterceptorRegistry.getClientInterceptors();
+        Set<ClientInterceptor> interceptorSet = new HashSet<>();
+        if (globalInterceptorList != null && !globalInterceptorList.isEmpty()) {
+            interceptorSet.addAll(globalInterceptorList);
         }
-        return channel;
+        if (interceptors != null && !interceptors.isEmpty()) {
+            interceptorSet.addAll(interceptors);
+        }
+        return ClientInterceptors.intercept(channel, Lists.newArrayList(interceptorSet));
     }
 }
