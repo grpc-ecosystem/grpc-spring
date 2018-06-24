98ccfa0ef0bd3eda39712539c3829153ba0d0671
Michael <yidongnan@gmail.com>
2018-06-24 16:01:28 +0800

    - remove deprecated function

--- grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/DiscoveryClientChannelFactory.java
+++ grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/DiscoveryClientChannelFactory.java
@@ -61,8 +61,10 @@ public class DiscoveryClientChannelFactory implements GrpcChannelFactory {
         GrpcChannelProperties channelProperties = properties.getChannel(name);
         NettyChannelBuilder builder = NettyChannelBuilder.forTarget(name)
                 .loadBalancerFactory(loadBalancerFactory)
-                .nameResolverFactory(new DiscoveryClientResolverFactory(client, this))
-                .usePlaintext(properties.getChannel(name).isPlaintext());
+                .nameResolverFactory(new DiscoveryClientResolverFactory(client, this));
+        if (properties.getChannel(name).isPlaintext()) {
+            builder.usePlaintext();
+        }
         if (channelProperties.isEnableKeepAlive()) {
             builder.keepAliveWithoutCalls(channelProperties.isKeepAliveWithoutCalls())
                     .keepAliveTime(channelProperties.getKeepAliveTime(), TimeUnit.SECONDS)
