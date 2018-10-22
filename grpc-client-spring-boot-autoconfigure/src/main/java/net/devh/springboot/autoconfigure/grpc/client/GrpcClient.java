dcd4b519c176e8d19969ba617588f52ca66f1127
Daniel Theuke <daniel.theuke@heuboe.de>
2018-10-22 15:37:44 +0200

    Minor improvements and javadocs for the client
    * Switch back to the grpc and secure default TLS
    * Change keep alive timeout to grpc default (which works better with
    firewalls)

--- grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/GrpcClient.java
+++ grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/GrpcClient.java
@@ -7,12 +7,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.grpc.Channel;
 import io.grpc.ClientInterceptor;
+import io.grpc.stub.AbstractStub;
 
 /**
- * User: Michael
- * Email: yidongnan@gmail.com
- * Date: 2016/12/7
+ * An annotation for fields of type {@link Channel} or subclasses of {@link AbstractStub}/gRPC
+ * client services. Annotated fields will be automatically populated by Spring.
+ *
+ * <p>
+ * <b>Note:</b> Fields that are annotated with this annotation should NOT be annotated with
+ * {@link Autowired} or {@link Inject} (conflict).
+ * </p>
+ *
+ * @author Michael (yidongnan@gmail.com)
+ * @since 2016/12/7
  */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
@@ -20,7 +33,31 @@ import io.grpc.ClientInterceptor;
 @Inherited
 public @interface GrpcClient {
 
+    /**
+     * The name of the grpc client. This name will be used to get the {@link GrpcChannelProperties
+     * config options} for this client.
+     *
+     * <p>
+     * <b>Example:</b> <code>@GrpcClient("myClient")</code> &lt;-&gt;
+     * <tt>grpc.client.myClient.port=9090</tt>
+     * </p>
+     *
+     * @return The name of the grpc client.
+     */
     String value();
 
+    /**
+     * A list of {@link ClientInterceptor}s that should be used with this client in addition to the
+     * globally defined ones. If a bean of the given type exists, it will be used; otherwise a new
+     * instance of that class will be created via no-args constructor.
+     *
+     * <p>
+     * <b>Note:</b> These interceptors will be applied after the global interceptors. But the
+     * interceptors that were applied last, will be called first.
+     * </p>
+     *
+     * @return A list of ClientInterceptors that should be used.
+     */
     Class<? extends ClientInterceptor>[] interceptors() default {};
+
 }
