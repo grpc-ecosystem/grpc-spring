80d252869f62f81f6fa45f230f4adca3e9b8c47e
Daniel Theuke <daniel.theuke@heuboe.de>
2018-11-27 10:08:45 +0100

    Automatically create actuator InfoContributor

---
+++ grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/boot/grpc/client/autoconfigure/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The Spring-Boot auto configuration classes that setup the gRPC client environment.
+ */
+
+package net.devh.boot.grpc.client.autoconfigure;
---
+++ grpc-server-spring-boot-autoconfigure/src/main/java/net/devh/boot/grpc/server/autoconfigure/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The Spring-Boot auto configuration classes that setup the gRPC server environment.
+ */
+
+package net.devh.boot.grpc.server.autoconfigure;
