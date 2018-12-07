7e199cf021d8ffbb3310c206d207e1d1f26b983a
Gregor Eeckels <gregor.eeckels@gmail.com>
2018-12-07 14:52:23 +0100

    Fix formatting

--- examples/security-grpc-bearerAuth-client/src/main/java/net/devh/boot/grpc/examples/security/client/GrpcClientController.java
+++ examples/security-grpc-bearerAuth-client/src/main/java/net/devh/boot/grpc/examples/security/client/GrpcClientController.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
