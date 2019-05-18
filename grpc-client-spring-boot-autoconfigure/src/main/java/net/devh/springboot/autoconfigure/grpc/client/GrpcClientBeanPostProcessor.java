7733960ab469806c48fdbe74fa17d147ad6078fd
ST-DDT <ST-DDT@gmx.de>
2019-05-18 23:35:52 +0200

    Backport the improvements made to the GrpcClientBeanPostProcessor.

--- grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/GrpcClientBeanPostProcessor.java
+++ grpc-client-spring-boot-autoconfigure/src/main/java/net/devh/springboot/autoconfigure/grpc/client/GrpcClientBeanPostProcessor.java
@@ -1,54 +1,89 @@
+/*
+ * Copyright (c) 2016-2019 Michael Zhang <yidongnan@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package net.devh.springboot.autoconfigure.grpc.client;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import static java.util.Objects.requireNonNull;
 
-import org.springframework.aop.framework.Advised;
-import org.springframework.aop.support.AopUtils;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.springframework.beans.BeanInstantiationException;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.InvalidPropertyException;
 import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.util.ReflectionUtils;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 import io.grpc.Channel;
 import io.grpc.ClientInterceptor;
-import lombok.SneakyThrows;
+import io.grpc.stub.AbstractStub;
 
 /**
- * User: Michael
- * Email: yidongnan@gmail.com
- * Date: 5/17/16
+ * This {@link BeanPostProcessor} searches for fields and methods in beans that are annotated with {@link GrpcClient}
+ * and sets them.
+ *
+ * @author Michael (yidongnan@gmail.com)
+ * @author Daniel Theuke (daniel.theuke@heuboe.de)
+ * @since 5/17/16
  */
-public class GrpcClientBeanPostProcessor implements org.springframework.beans.factory.config.BeanPostProcessor {
+public class GrpcClientBeanPostProcessor implements BeanPostProcessor {
 
-    private Map<String, List<Class>> beansToProcess = Maps.newHashMap();
+    private final ApplicationContext applicationContext;
 
-    @Autowired
-    private DefaultListableBeanFactory beanFactory;
+    // Lazy initialized when needed to avoid overly eager creation of that factory,
+    // which might break proper bean setup
+    private GrpcChannelFactory channelFactory = null;
 
-    @Autowired
-    private GrpcChannelFactory channelFactory;
-
-    public GrpcClientBeanPostProcessor() {
+    public GrpcClientBeanPostProcessor(final ApplicationContext applicationContext) {
+        this.applicationContext = requireNonNull(applicationContext, "applicationContext");
     }
 
     @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        Class clazz = bean.getClass();
+    public Object postProcessBeforeInitialization(final Object bean, final String beanName) throws BeansException {
+        Class<?> clazz = bean.getClass();
         do {
-            for (Field field : clazz.getDeclaredFields()) {
-                if (field.isAnnotationPresent(GrpcClient.class)) {
-                    if (!beansToProcess.containsKey(beanName)) {
-                        beansToProcess.put(beanName, new ArrayList<Class>());
+            for (final Field field : clazz.getDeclaredFields()) {
+                final GrpcClient annotation = AnnotationUtils.findAnnotation(field, GrpcClient.class);
+                if (annotation != null) {
+                    ReflectionUtils.makeAccessible(field);
+                    ReflectionUtils.setField(field, bean, processInjectionPoint(field, field.getType(), annotation));
+                }
+            }
+            for (final Method method : clazz.getDeclaredMethods()) {
+                final GrpcClient annotation = AnnotationUtils.findAnnotation(method, GrpcClient.class);
+                if (annotation != null) {
+                    final Class<?>[] paramTypes = method.getParameterTypes();
+                    if (paramTypes.length != 1) {
+                        throw new BeanDefinitionStoreException(
+                                "Method " + method + " doesn't have exactly one parameter.");
                     }
-                    beansToProcess.get(beanName).add(clazz);
+                    ReflectionUtils.makeAccessible(method);
+                    ReflectionUtils.invokeMethod(method, bean,
+                            processInjectionPoint(method, paramTypes[0], annotation));
                 }
             }
             clazz = clazz.getSuperclass();
@@ -56,48 +91,114 @@ public class GrpcClientBeanPostProcessor implements org.springframework.beans.fa
         return bean;
     }
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (beansToProcess.containsKey(beanName)) {
-            Object target = getTargetBean(bean);
-            for (Class clazz : beansToProcess.get(beanName)) {
-                for (Field field : clazz.getDeclaredFields()) {
-                    GrpcClient annotation = AnnotationUtils.getAnnotation(field, GrpcClient.class);
-                    if (null != annotation) {
+    /**
+     * Processes the given injection point and computes the appropriate value for the injection.
+     *
+     * @param <T> The type of the value to be injected.
+     * @param injectionTarget The target of the injection.
+     * @param injectionType The class that will be used to compute injection.
+     * @param annotation The annotation on the target with the metadata for the injection.
+     * @return The value to be injected for the given injection point.
+     */
+    protected <T> T processInjectionPoint(final Member injectionTarget, final Class<T> injectionType,
+            final GrpcClient annotation) {
+        final List<ClientInterceptor> interceptors = interceptorsFromAnnotation(annotation);
+        final String name = annotation.value();
+        final Channel channel;
+        try {
+            channel = getChannelFactory().createChannel(name, interceptors);
+            if (channel == null) {
+                throw new IllegalStateException("Channel factory created a null channel for " + name);
+            }
+        } catch (final RuntimeException e) {
+            throw new IllegalStateException("Failed to create channel: " + name, e);
+        }
 
-                        List<ClientInterceptor> list = Lists.newArrayList();
-                        for (Class<? extends ClientInterceptor> clientInterceptorClass : annotation.interceptors()) {
-                            ClientInterceptor clientInterceptor;
-                            if (beanFactory.getBeanNamesForType(ClientInterceptor.class).length > 0) {
-                                clientInterceptor = beanFactory.getBean(clientInterceptorClass);
-                            } else {
-                                try {
-                                    clientInterceptor = clientInterceptorClass.newInstance();
-                                } catch (Exception e) {
-                                    throw new BeanCreationException("Failed to create interceptor instance", e);
-                                }
-                            }
-                            list.add(clientInterceptor);
-                        }
+        final T value = valueForMember(name, injectionTarget, injectionType, channel);
+        if (value == null) {
+            throw new IllegalStateException(
+                    "Injection value is null unexpectedly for " + name + " at " + injectionTarget);
+        }
+        return value;
+    }
 
-                        Channel channel = channelFactory.createChannel(annotation.value(), list);
-                        ReflectionUtils.makeAccessible(field);
-                        ReflectionUtils.setField(field, target, channel);
-                    }
+    /**
+     * Lazy getter for the {@link GrpcChannelFactory}.
+     *
+     * @return The grpc channel factory to use.
+     */
+    private GrpcChannelFactory getChannelFactory() {
+        if (this.channelFactory == null) {
+            final GrpcChannelFactory factory = this.applicationContext.getBean(GrpcChannelFactory.class);
+            this.channelFactory = factory;
+            return factory;
+        }
+        return this.channelFactory;
+    }
+
+    /**
+     * Gets or creates the {@link ClientInterceptor}s that are referenced in the given annotation.
+     *
+     * <p>
+     * <b>Note:</b> This methods return value does not contain the global client interceptors because they are handled
+     * by the {@link GrpcChannelFactory}.
+     * </p>
+     *
+     * @param annotation The annotation to get the interceptors for.
+     * @return A list containing the interceptors for the given annotation.
+     * @throws BeansException If the referenced interceptors weren't found or could not be created.
+     */
+    protected List<ClientInterceptor> interceptorsFromAnnotation(final GrpcClient annotation) throws BeansException {
+        final List<ClientInterceptor> list = Lists.newArrayList();
+        for (final Class<? extends ClientInterceptor> interceptorClass : annotation.interceptors()) {
+            final ClientInterceptor clientInterceptor;
+            if (this.applicationContext.getBeanNamesForType(ClientInterceptor.class).length > 0) {
+                clientInterceptor = this.applicationContext.getBean(interceptorClass);
+            } else {
+                try {
+                    clientInterceptor = interceptorClass.getConstructor().newInstance();
+                } catch (final Exception e) {
+                    throw new BeanCreationException("Failed to create interceptor instance", e);
                 }
             }
+            list.add(clientInterceptor);
         }
-        return bean;
+        return list;
     }
 
-    @SneakyThrows
-    private Object getTargetBean(Object bean) {
-        Object target = bean;
-        while (AopUtils.isAopProxy(target)) {
-            target = ((Advised) target).getTargetSource().getTarget();
+    /**
+     * Creates the instance to be injected for the given member.
+     *
+     * @param name The name that was used to create the channel.
+     * @param <T> The type of the instance to be injected.
+     * @param injectionTarget The target member for the injection.
+     * @param injectionType The class that should injected.
+     * @param channel The channel that should be used to create the instance.
+     * @return The value that matches the type of the given field.
+     * @throws BeansException If the value of the field could not be created or the type of the field is unsupported.
+     */
+    protected <T> T valueForMember(final String name, final Member injectionTarget, final Class<T> injectionType,
+            final Channel channel) throws BeansException {
+        if (Channel.class.equals(injectionType)) {
+            return injectionType.cast(channel);
+        } else if (AbstractStub.class.isAssignableFrom(injectionType)) {
+            try {
+                @SuppressWarnings("unchecked")
+                final Class<? extends AbstractStub<?>> stubClass =
+                        (Class<? extends AbstractStub<?>>) injectionType.asSubclass(AbstractStub.class);
+                final Constructor<? extends AbstractStub<?>> constructor =
+                        ReflectionUtils.accessibleConstructor(stubClass, Channel.class);
+                AbstractStub<?> stub = constructor.newInstance(channel);
+                return injectionType.cast(stub);
+            } catch (final NoSuchMethodException | InstantiationException | IllegalAccessException
+                    | IllegalArgumentException | InvocationTargetException e) {
+                throw new BeanInstantiationException(injectionType,
+                        "Failed to create gRPC client for : " + injectionTarget, e);
+            }
+        } else {
+            throw new InvalidPropertyException(injectionTarget.getDeclaringClass(), injectionTarget.getName(),
+                    "Unsupported type " + injectionType.getName());
         }
-        return target;
     }
-
 
 }
