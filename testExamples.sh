c46405f8d2da4cca5bde9f549eb9639b14b942c5
ST-DDT <ST-DDT@gmx.de>
2020-02-23 12:05:18 +0100

    Improve test script
    - Don't wait that long if nothing happens in the meantime
    - Fix discovery refresh error
    - Add some more comments

--- testExamples.sh
+++ testExamples.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e # Fail on error
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT # Kill subprocesses on exit
 ./gradlew clean --console=plain
 ./gradlew build --console=plain
 sleep 2s
@@ -11,10 +12,10 @@ localTest() {
 	# Run environment
 	./gradlew :example:local-grpc-server:bootRun -x jar -x classes --console=plain &
 	LOCAL_SERVER=$!
-	sleep 10s
+	sleep 10s # Wait for the server to start
 	./gradlew :example:local-grpc-client:bootRun -x jar -x classes --console=plain &
 	LOCAL_CLIENT=$!
-	sleep 30s
+	sleep 30s # Wait for the client to start and the server to be ready
 
 	# Test
 	RESPONSE=$(curl -s localhost:8080/)
@@ -23,13 +24,13 @@ localTest() {
 	EXPECTED=$(echo -e "Hello ==> Michael")
 	echo "Expected:"
 	echo "$EXPECTED"
-	sleep 2s
+	sleep 1s # Give the user a chance to look at the result
 
 	# Shutdown
 	echo "Triggering shutdown"
 	kill -s TERM $LOCAL_SERVER
 	kill -s TERM $LOCAL_CLIENT
-	sleep 5s
+	sleep 1s # Wait for the shutdown logs to pass 
 
 	# Verify
 	if [ "$RESPONSE" = "$EXPECTED" ]; then
@@ -42,7 +43,6 @@ localTest() {
 		echo "#-----------------------#"
 		exit 1
 	fi
-	sleep 30s
 }
 
 ## Cloud
@@ -52,7 +52,7 @@ cloudTest() {
 	# Run environment
 	./gradlew :example:cloud-eureka-server:bootRun -x jar -x classes --console=plain &
 	EUREKA=$!
-	sleep 10s
+	sleep 10s # Wait for the server to start
 
 	mkdir -p zipkin
 	cd zipkin
@@ -62,16 +62,16 @@ cloudTest() {
 	fi
 	java -jar zipkin.jar &
 	ZIPKIN=$!
-	sleep 10s
+	sleep 10s # Wait for the server to start
 	cd ..
 
 	./gradlew :example:cloud-grpc-server:bootRun -x jar -x classes --console=plain &
 	CLOUD_SERVER=$!
-	sleep 30s
+	sleep 10s # Wait for the server to start
 
 	./gradlew :example:cloud-grpc-client:bootRun -x jar -x classes --console=plain &
 	CLOUD_CLIENT=$!
-	sleep 30s
+	sleep 30s # Wait for the client to start and the server to be ready
 
 	# Test
 	RESPONSE=$(curl -s localhost:8080/)
@@ -80,17 +80,18 @@ cloudTest() {
 	EXPECTED=$(echo -e "Hello ==> Michael")
 	echo "Expected:"
 	echo "$EXPECTED"
-	sleep 2s
+	sleep 1s # Give the user a chance to look at the result
 
 	# Crash server
 	kill -s TERM $CLOUD_SERVER
 	echo "The server crashed (expected)"
-	sleep 10s
+	sleep 1s # Wait for the shutdown logs to pass
 
 	# and restart server
 	./gradlew :example:cloud-grpc-server:bootRun -x jar -x classes --console=plain &
 	CLOUD_SERVER=$!
-	sleep 60s
+	sleep 30s # Wait for the server to start
+	sleep 60s # Wait for the discovery service to refresh
 	
 	# Test again
 	RESPONSE2=$(curl -s localhost:8080/)
@@ -99,7 +100,7 @@ cloudTest() {
 	EXPECTED=$(echo -e "Hello ==> Michael")
 	echo "Expected:"
 	echo "$EXPECTED"
-	sleep 2s
+	sleep 1s # Give the user a chance to look at the result
 
 	# Shutdown
 	echo "Triggering shutdown"
@@ -107,7 +108,7 @@ cloudTest() {
 	kill -s TERM $ZIPKIN
 	kill -s TERM $CLOUD_SERVER
 	kill -s TERM $CLOUD_CLIENT
-	sleep 5s
+	sleep 1s # Wait for the shutdown logs to pass
 
 	# Verify part 1
 	if [ "$RESPONSE" = "$EXPECTED" ]; then
@@ -141,10 +142,10 @@ securityBasicAuthTest() {
 	# Run environment
 	./gradlew :example:security-grpc-server:bootRun -x jar -x classes --console=plain &
 	LOCAL_SERVER=$!
-	sleep 10s
+	sleep 10s # Wait for the server to start
 	./gradlew :example:security-grpc-client:bootRun -x jar -x classes --console=plain &
 	LOCAL_CLIENT=$!
-	sleep 30s
+	sleep 30s # Wait for the client to start and the server to be ready
 
 	# Test
 	RESPONSE=$(curl -s localhost:8080/)
@@ -153,13 +154,13 @@ securityBasicAuthTest() {
 	EXPECTED=$(echo -e "Input:\n- name: Michael (Changeable via URL param ?name=X)\nRequest-Context:\n- auth user: user (Configure via application.yml)\nResponse:\nHello ==> Michael")
 	echo "Expected:"
 	echo "$EXPECTED"
-	sleep 2s
+	sleep 1s # Give the user a chance to look at the result
 
 	# Shutdown
 	echo "Triggering shutdown"
 	kill -s TERM $LOCAL_SERVER
 	kill -s TERM $LOCAL_CLIENT
-	sleep 5s
+	sleep 1s # Wait for the shutdown logs to pass
 
 	# Verify
 	if [ "$RESPONSE" = "$EXPECTED" ]; then
@@ -181,10 +182,10 @@ securityBearerAuthTest() {
 	# Run environment
 	./gradlew :example:security-grpc-bearerAuth-server:bootRun -x jar -x classes --console=plain &
 	LOCAL_SERVER=$!
-	sleep 10s
+	sleep 10s # Wait for the server to start
 	./gradlew :example:security-grpc-bearerAuth-client:bootRun -x jar -x classes --console=plain &
 	LOCAL_CLIENT=$!
-	sleep 30s
+	sleep 30s # Wait for the client to start and the server to be ready
 
 	# Test
 	RESPONSE=$(curl -s localhost:8080/)
@@ -193,13 +194,13 @@ securityBearerAuthTest() {
 	EXPECTED=$(echo -e "Input:\nMessage: test, Bearer Token is configured in SecurityConfiguration Class\nResponse:\nHello ==> test")
 	echo "Expected:"
 	echo "$EXPECTED"
-	sleep 2s
+	sleep 1s # Give the user a chance to look at the result
 
 	# Shutdown
 	echo "Triggering shutdown"
 	kill -s TERM $LOCAL_SERVER
 	kill -s TERM $LOCAL_CLIENT
-	sleep 5s
+	sleep 1s # Wait for the shutdown logs to pass
 
 	# Verify
 	if [ "$RESPONSE" = "$EXPECTED" ]; then
