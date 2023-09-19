13d1a10ed0ecef1453b142e8949998775a34998d
Michael Zhang <michael@michaeldemac-mini.local>
2023-09-19 09:17:27 +0800

    add spring boot 3.0 example

---
+++ examples/springboot3-local-grpc-client/src/main/java/net/devh/boot/grpc/examples/local/client/LogGrpcInterceptor.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2016-2021 Michael Zhang <yidongnan@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package net.devh.boot.grpc.examples.local.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.ForwardingClientCallListener;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.Status;
+import net.devh.boot.grpc.client.interceptor.GrpcGlobalClientInterceptor;
+
+/**
+ * Example {@link ClientInterceptor} that logs all called methods in INFO log level, also request and response messages,
+ * headers, trailers and interaction status in DEBUG log level. In this example it is added to Spring's application
+ * context via {@link GlobalClientInterceptorConfiguration}, but is also possible to directly annotate this class with
+ * {@link GrpcGlobalClientInterceptor}.
+ *
+ * @author Michael (yidongnan@gmail.com)
+ * @since 2016/12/8
+ */
+public class LogGrpcInterceptor implements ClientInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(LogGrpcInterceptor.class);
+
+    @Override
+    public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
+            MethodDescriptor<ReqT, RespT> method,
+            CallOptions callOptions,
+            Channel next) {
+
+        log.info("Received call to {}", method.getFullMethodName());
+        return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
+
+            @Override
+            public void sendMessage(ReqT message) {
+                log.debug("Request message: {}", message);
+                super.sendMessage(message);
+            }
+
+            @Override
+            public void start(Listener<RespT> responseListener, Metadata headers) {
+                super.start(
+                        new ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(responseListener) {
+                            @Override
+                            public void onMessage(RespT message) {
+                                log.debug("Response message: {}", message);
+                                super.onMessage(message);
+                            }
+
+                            @Override
+                            public void onHeaders(Metadata headers) {
+                                log.debug("gRPC headers: {}", headers);
+                                super.onHeaders(headers);
+                            }
+
+                            @Override
+                            public void onClose(Status status, Metadata trailers) {
+                                log.info("Interaction ends with status: {}", status);
+                                log.info("Trailers: {}", trailers);
+                                super.onClose(status, trailers);
+                            }
+                        }, headers);
+            }
+        };
+    }
+
+}
