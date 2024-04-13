93058372a0b78aa51e6005122d04039942d8affe
ST-DDT <ST-DDT@gmx.de>
2024-04-13 09:14:15 +0200

    Update grpc-server-spring-boot-starter/src/main/java/net/devh/boot/grpc/server/security/interceptors/ManagerResolverAuthenticatingServerInterceptor.java

--- grpc-server-spring-boot-starter/src/main/java/net/devh/boot/grpc/server/security/interceptors/ManagerResolverAuthenticatingServerInterceptor.java
+++ grpc-server-spring-boot-starter/src/main/java/net/devh/boot/grpc/server/security/interceptors/ManagerResolverAuthenticatingServerInterceptor.java
@@ -68,8 +68,6 @@ public class ManagerResolverAuthenticatingServerInterceptor extends AbstractAuth
         this.authenticationManagerResolver =
                 requireNonNull(authenticationManagerResolver, "authenticationManagerResolver");
     }
-
-
     @Override
     protected AuthenticationManager getAuthenticationManager(
             final ServerCall<?, ?> call,
